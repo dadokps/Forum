27a4f64e14df3410bb45a0815c4630ef3ae4ad6e
ALJOVIC Damir <Damir.Aljovic@qualysoft.com>
2019-07-05 18:33:44 +0200

    Global Search

---
+++ LambdaForum/Controllers/SearchController.cs
@@ -0,0 +1,70 @@
+﻿using System.Globalization;
+using System.Linq;
+using LambdaForums.Data.Models;
+using LambdaForum.Models.Forum;
+using LambdaForum.Models.Post;
+using LambdaForum.Models.Search;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LambdaForum.Controllers
+{
+    public class SearchController : Controller
+    {
+        private readonly IPost _postService;
+
+        public SearchController(IPost postService)
+        {
+            _postService = postService;
+        }
+
+        public IActionResult Results(string searchQuery)
+        {
+            var posts = _postService.GetFilteredPosts(searchQuery).ToList();
+            var noResults = (!string.IsNullOrEmpty(searchQuery) && !posts.Any());
+
+            var postListings = posts.Select(post => new PostListingModel
+            {
+                Id = post.Id,
+                Forum = BuildForumListing(post),
+                AuthorName = post.User.UserName,
+                AuthorId = post.User.Id,
+                AuthorRating = post.User.Rating,
+                Title = post.Title,
+                DatePosted = post.Created.ToString(CultureInfo.InvariantCulture),
+                RepliesCount = post.Replies.Count()
+            }).OrderByDescending(post => post.DatePosted);
+
+            var model = new SearchResultModel
+            {
+                EmptySearchResults = noResults,
+                Posts = postListings,
+                SearchQuery = searchQuery,
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Search(string searchQuery)
+        {
+            return RedirectToAction("Results", new { searchQuery });
+        }
+
+        private static ForumListingModel BuildForumListing(Forum forum)
+        {
+            return new ForumListingModel
+            {
+                Id = forum.Id,
+                ImageUrl = forum.ImageUrl,
+                Title = forum.Title,
+                Description = forum.Description
+            };
+        }
+
+        private static ForumListingModel BuildForumListing(Post post)
+        {
+            var forum = post.Forum;
+            return BuildForumListing(forum);
+        }
+    }
+}
---
+++ LambdaForum/Models/Search/SearchResultModel.cs
@@ -0,0 +1,12 @@
+﻿using LambdaForum.Models.Post;
+using System.Collections.Generic;
+
+namespace LambdaForum.Models.Search
+{
+    public class SearchResultModel
+    {
+        public IEnumerable<PostListingModel> Posts { get; set; }
+        public string SearchQuery { get; set; }
+        public bool EmptySearchResults { get; set; }
+    }
+}
